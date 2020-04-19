9a49f847bb5f39b38dc1dc4637131a844228b9d4
Daniil <d.a.sherkozhukhov1995@gmail.com>
2020-04-19 20:47:27 +0300

    HomeWork 3

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 'use strict';
 
-let money = +prompt('Ваш бюджет на месяц?', '');
-let time = prompt('Введите дату в формате YYYY-MM-DD', '');
+let money, time; //объявляем глобально
+
+function start() {
+	money = +prompt('Ваш бюджет на месяц?', '');
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+	while(isNaN(money) || money == '' || money == null) { //команда isNan возвращает try в том случае, если у нас туда попадают не цифры
+		money = +prompt('Ваш бюджет на месяц?', '');
+	}
+
+} 
+start();
+
 
 let appData = {
 	budget: money,
@@ -9,77 +20,64 @@ let appData = {
 	expenses: {},
 	optionalExpenses: {},
 	income: [],
-	savings: false
-}	
-
-/* for (let i = 0; i < 2; i++) {
-	let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
-		b = prompt('Во сколько обойдется?', '');
-
-	if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
-		&& a != '' && b != '' && a.length < 50) { //проверка, что данные явл-ся строкой, и будет проверять на отмену (Null), т.е. если user нажмет на отмену то действие не выполнится, и чтобы пустой строкой тоже не вводилось
-		
-		console.log('done');
-
-		appData.expenses[a] = b;
-	} else {
-		console.log('bad result');
-		i--;
-	}
-}; */
-
-// Цикл while
-/* let i = 0;
-while(i < 2) {
-	let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
-		b = prompt('Во сколько обойдется?', '');
-
-	if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
-		&& a != '' && b != '' && a.length < 50) { //проверка, что данные явл-ся строкой, и будет проверять на отмену (Null), т.е. если user нажмет на отмену то действие не выполнится, и чтобы пустой строкой тоже не вводилось
-		
-		console.log('done');
-
-		appData.expenses[a] = b;
-	} else {
-		console.log('bad result');
-		i--;
-	}
-	i++;
-} */
-
-//цикл do while
-let i = 0
-do {
-	let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
-		b = prompt('Во сколько обойдется?', '');
-
-	if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
-		&& a != '' && b != '' && a.length < 50) { //проверка, что данные явл-ся строкой, и будет проверять на отмену (Null), т.е. если user нажмет на отмену то действие не выполнится, и чтобы пустой строкой тоже не вводилось
-		
-		console.log('done');
-
-		appData.expenses[a] = b;
-	} else {
-		console.log('bad result');
-		i--;
-	}
-	i++;
-}
-while(i < 2);
+	savings: true
+};
 
 
-appData.moneyPerDay = appData.budget / 30
+function chooseExpenses() {
+	for (let i = 0; i < 2; i++) {
+		let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+			b = prompt('Во сколько обойдется?', '');
 	
-alert('Ежедневный бюджет: ' + appData.moneyPerDay);
-
-if(appData.moneyPerDay < 100) { //выводит уровень достатка
-	console.log('минимальный уровень достатка');
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-	console.log('средний уровень достатка');
-} else if (appData.moneyPerDay > 2000) {
-	console.log('высокий уровень достатка');
-} else {
-	console.log('Произошла ошибка');
+		if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null
+			&& a != '' && b != '' && a.length < 50) { //проверка, что данные явл-ся строкой, и будет проверять на отмену (Null), т.е. если user нажмет на отмену то действие не выполнится, и чтобы пустой строкой тоже не вводилось
+			appData.expenses[a] = b;
+		} else {
+			i--;
+		}
+	};
 }
+chooseExpenses();
 
-console.log(appData);
+//расчет дневного бюджета
+function detectDayBudget() {
+	appData.moneyPerDay = (appData.budget / 30).toFixed();
+	alert('Ваш бюджет на день составляет: ' + appData.moneyPerDay + 'руб.');
+}
+detectDayBudget();
+
+//расчет уровня достатка
+function detectLevel() {
+	if(appData.moneyPerDay < 100) { //выводит уровень достатка
+		console.log('минимальный уровень достатка');
+	} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+		console.log('средний уровень достатка');
+	} else if (appData.moneyPerDay > 2000) {
+		console.log('высокий уровень достатка');
+	} else {
+		console.log('Произошла ошибка');
+	}
+}
+detectLevel();
+
+//доход от накоплений
+function checkSavings() {
+	if (appData.savings == true) {
+		let save = +prompt('Какова сумма накоплений?'),
+			percent = +prompt('Под какой процент?');
+
+			appData.monthIncome = save/100/12*percent;
+			alert('Доход в месяц с вашего депозита: ' + appData.monthIncome);
+	}
+}
+checkSavings();
+
+//определение необязательных расходов
+function chooseOptExpenses() {
+	for (let i = 0; i < 3; i++) {
+		let questionOptExpenses = prompt('Статья необязательных расходов?');
+		appData.optionalExpenses[i] = questionOptExpenses;
+		console.log(appData.optionalExpenses);
+	}
+}
+chooseOptExpenses();
